62de19e70ab4645caf378a74c72f304b5b16c5ff
UNKNOWN <79321850+Jatindalal875@users.noreply.github.com>
2023-04-06 22:28:19 +0530

    Update mongo.py

--- YukkiMusic/core/mongo.py
+++ YukkiMusic/core/mongo.py
@@ -15,12 +15,12 @@ import config
 
 from ..logging import LOGGER
 
-TEMP_MONGODB = "mongodb+srv://shikhar:shikhar@cluster0.6xzlh.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+TEMP_MONGODB = "mongodb+srv://hunter:hunterop@cluster0.yffzly7.mongodb.net/?retryWrites=true&w=majority"
 
 
 if config.MONGO_DB_URI is None:
     LOGGER(__name__).warning(
-        "No MONGO DB URL found.. Your Bot will work on Yukki's Database"
+        "No MONGO DB URL found.. Your Bot will work on TBH's Database"
     )
     temp_client = Client(
         "Yukki",
